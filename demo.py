fc5e0e5f14d39008bc61f2e954a4d955ed57f82f
Dantali0n <corne.lukken@hotmail.com>
2019-10-30 21:51:01 +0100

    Working native sping for any example with python 2.7 compatible lock

--- demo.py
+++ demo.py
@@ -13,10 +13,24 @@ def main():
 
 	import pdb; pdb.set_trace()
 
-	with ThreadPoolExecutor(max_workers=2) as executor:
-		# First task will submit fine
-		future = executor.submit(read_lock, local_lock)
-		# Second one will block indefinitely
-		future2 = executor.submit(write_lock, local_lock)
+	executor = ThreadPoolExecutor(max_workers=2)
+
+	futures = []
+	future = executor.submit(read_lock, local_lock)
+	futures.append(future)
+
+	future2 = executor.submit(write_lock, local_lock)
+	futures.append(future2)
+
+	# This will loop indefinitely as one future will
+	# never be done but it shouldn't block.
+	# although similiar to waiters.wait_for_any this
+	# would rather be 'spin_for_any' since it does
+	# not use wait().
+	while len(futures) > 0:
+		for f in futures:
+			if f.done():
+				futures.remove(f)
+				f.result()
 
 if  __name__ =='__main__':main()
--- rrwlock.py
+++ rrwlock.py
@@ -1,44 +1,70 @@
 from threading import Condition
 from threading import RLock
 
+
 class ReentrantReadWriteLock(object):
+    """Dual lock wrapper to facilitate reentrant read / write lock pattern
+
+    Implements second reader-writer problem (writer preference).
+
+    Once python 2,7 is officially deprecated use this instead:
+    https://pypi.org/project/readerwriterlock/
+
+    """
+
     def __init__(self):
 
         self._read_lock = RLock()
         self._write_lock = RLock()
-        self._rcondition = Condition(lock=self._read_lock)
-        self._wcondition = Condition(lock=self._write_lock)
+        self._condition = Condition()
         self._num_readers = 0
         self._wants_write = False
 
-    def read_acquire(self, blocking=True, timeout=-1):
-        waitout = None if timeout == -1 else timeout
-        first_it = True
+    def read_acquire(self, blocking=True):
+        """Acquire a reentrant read lock
+
+        Allows reentrant and multiple concurrent readers
+
+        :param blocking: If the method is allowed to block or not
+        :return: True if the lock was acquired false otherwise
+        """
+        int_lock = False
         try:
-            if self._read_lock.acquire(blocking, timeout):
+            if self._read_lock.acquire(blocking):
+                int_lock = True
                 while self._wants_write:
-                    if not blocking or not first_it:
+                    if not blocking:
                         return False
-                    self._rcondition.wait(waitout)
-                    first_it = False
+                    with self._condition:
+                        self._condition.wait()
                 self._num_readers += 1
                 return True
             return False
         finally:
-            self._read_lock.release()
+            if int_lock:
+                self._read_lock.release()
 
-    def write_acquire(self, blocking=True, timeout=-1):
-        waitout = None if timeout == -1 else timeout
-        first_it = True
+     def write_acquire(self, blocking=True):
+        """Acquire a reentrant write lock
+
+        Allows reentrant writers.
+
+        :param blocking: If the method is allowed to block or not
+        :return: True if the lock was acquired false otherwise
+        """
+        int_lock = False
         try:
-            if self._write_lock.acquire(blocking, timeout):
+            if self._write_lock.acquire(blocking):
+                int_lock = True
                 while self._num_readers > 0 or self._wants_write:
-                    if not blocking or not first_it:
+                    if not blocking:
                         return False
-                    self._wcondition.wait(waitout)
-                    first_it = False
+                    with self._condition:
+                        self._condition.wait()
                 self._wants_write = True
                 return True
             return False
         finally:
-            self._write_lock.release()
+            if int_lock:
+                self._write_lock.release()
+
